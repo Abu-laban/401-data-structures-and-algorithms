9b41f1464c39b15d4c915d187082c428befbaf5e
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-08-17 23:49:27 +0300

    finish challenge-03

---
+++ javascript/code-challenges/array-binary-search/challenges-03.test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 3 - BinarySearch
+
+Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the value of the search key, or -1 if the element is not in the array.
+
+For example:
+
+Input
+
+[4, 8, 15, 16, 23, 42], 15
+
+
+Output
+
+2
+
+
+
+------------------------------------------------------------------------------------------------ */
+
+let BinarySearch = (arr, num) => {
+
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] === num) {
+            return index;
+        }
+    }
+    return -1;
+};
+
+/* ------------------------------------------------------------------------------------------------
+TESTS
+
+All the code below will verify that your functions are working to solve the challenges.
+
+DO NOT CHANGE any of the below code.
+
+Run your tests from the console: jest challenges-01.test.js
+
+------------------------------------------------------------------------------------------------ */
+
+describe('Testing challenge 3', () => {
+    it('It returns an the index of numbers or -1 ', () => {
+        expect(BinarySearch([4, 8, 15, 16, 23, 42], 15)).toStrictEqual(2);
+
+    });
+});
+
